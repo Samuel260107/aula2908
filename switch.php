ffa4feb58da0b64502957ebbbdde2f10b67c33b8
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-09-29 14:19:58 -0300

    aa

---
+++ switch.php
@@ -0,0 +1,26 @@
+<?php
+$numero = 5;
+$nome = "";
+
+switch($numero){
+    case 4:
+        echo "O número é igual a 4 <br>";
+        break;
+    case 4:
+         echo "O número é igual a 5 <br>";
+        break;
+    default:
+         echo "O número não corresponde aos valores <br>";
+}
+    switch($nome){
+    case "Rosana":
+        echo "O nome é Rosana";
+        break;
+    case "Isis":
+        echo "O nome é Isis";
+        break;
+    default:
+    echo "O nome não foi encontrado";
+    }
+    
+?>
