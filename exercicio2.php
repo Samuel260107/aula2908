19d59000bb5ede3449fec8d503cec25cb91e81a7
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-09-29 09:53:57 -0300

    commit do samuel

---
+++ exercicio2.php
@@ -0,0 +1,13 @@
+<?php
+$a = 5;
+$b = 8;
+$c = "Samuel";
+
+if(is_int($a)){
+    $d = $a * 2;
+} if ($d > 100){
+    echo "<br> $d é maior que 100";
+} else {
+    echo "<br> $d não é maior que 100";
+}
+?>
